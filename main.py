6845d1bc7e4bb546c334a43d2f4c21aa35f007bf
SantyCastill0 <santiago.castillo@alumnos.frm.utn.edu.ar>
2022-05-26 16:11:52 -0300

    Metodo main

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-print("Hello World!!")
-print("Clase Main :D")
-print("gfhkmgf")
-print("asdsad")
+import pygame
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    game = Juego(WIN)
+
+    while run:
+        clock.tick(FPS)
+
+        if game.ganador() != None:
+            print(game.ganador())
+            run = False
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                fil, col = get_fil_col_from_mouse(pos)
+                game.select(fil, col)
+        game.update()
+    pygame.quit()
+main()
