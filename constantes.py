9b66fa6781f6cb26216a5fc46eb42943483c7fe5
SantyCastill0 <santiago.castillo@alumnos.frm.utn.edu.ar>
2022-05-29 17:50:23 -0300

    Creacion de la clase constantes
    Se definio los colores que se usaran, usamos RGB, las medidas de las filas, columnas, el ancho y el alto

---
+++ constantes.py
@@ -0,0 +1,20 @@
+import pygame
+
+FILAS, COLUMNAS = 8, 8
+ANCHO, ALTURA = 800, 800
+
+#Agregar corona
+#CORONA = corona.png
+
+TAMANIOCUADRADOTOTAL = ANCHO//COLUMNAS
+
+VIOLETA = (38, 43, 26)
+
+BLANCO = (224, 176, 255)
+
+NEGRO = (20, 20, 20)
+
+GRIS = (128, 128, 128)
+
+AZUL = (59, 131, 189)
+
