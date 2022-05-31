df7ca81c15be53b8d453533b8182bf8d09f658a9
MACA <macacalderini@gmail.com>
2022-05-31 17:39:38 -0300

    Cuadro/Tablero
    Signed-off-by: MACA <macacalderini@gmail.com>

--- tablero.py
+++ tablero.py
@@ -8,3 +8,27 @@ class Tablero:
         self.tablero = []
         self.violetaIzq = self.blancoIzq = 12
         self.violetaRey = self.blancoRey = 0
+
+    def crearCuadrado(self, ganar):
+        ganar.fill(NEGRO)
+
+        for filas in range(FILAS):
+            for columnas in range(filas % 2, COLUMNAS, 2):
+                pygame.draw.rect(ganar, VIOLETA, (
+                filas * TAMANIOCUADRADOTOTAL, columnas * TAMANIOCUADRADOTOTAL, TAMANIOCUADRADOTOTAL,
+                TAMANIOCUADRADOTOTAL))
+
+    def crearTablero(self):
+        for filas in range(FILAS):
+            self.tablero.append([])
+
+            for columnas in range(COLUMNAS):
+                if columnas % 2 == ((filas + 1) % 2):
+                    if filas < 3:
+                        self.tablero[filas].append(Piezas(filas, columnas, BLANCO))
+                    elif filas > 4:
+                        self.tablero[filas].append(Piezas(filas, columnas, VIOLETA))
+                    else:
+                        self.tablero[filas].append(0)
+                else:
+                    self.tablero[filas].append(0)
