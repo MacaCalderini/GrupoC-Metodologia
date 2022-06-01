eb803d2b7496dfed562ed663bc344079bdd26c99
Ignacio <ignacio.barolo@alumnos.frm.utn.edu.ar>
2022-06-01 12:27:03 -0300

    Agregación de variables constantes en clase Menu

---
+++ menu.py
@@ -0,0 +1,18 @@
+import pygame, sys
+
+pygame.init()
+
+# variables
+WIDTH = 1080
+HEIGHT = 720
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+FONT = pygame.font.Font(None, 32)
+Lfont = pygame.font.Font(None, 96)
+Mfont = pygame.font.Font(None, 60)
+# colors
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+gray = (100, 100, 100)
